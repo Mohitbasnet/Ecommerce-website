c2fd87d25622094412521033d30b1e14fee00e5e
banset <mohitbasnet80@gmail.com>
2024-04-02 15:32:45 +0545

    added Dealadmin

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -11,7 +11,9 @@ class CartInline(admin.TabularInline):
 
 class DealInline(admin.TabularInline):
     model = Deal.user.through
-    
+
+class ProductInCartInline(admin.TabularInline):
+    model = ProductInCart
 
 class UserAdmin(UserAdmin):
     model = User
@@ -42,11 +44,44 @@ class UserAdmin(UserAdmin):
     search_fields = ('username',)     #search_filter for search bar
     ordering = ('username',)
 
+@admin.register(Cart) # we can register cartadmin like this also
+class CartAdmin(admin.ModelAdmin):
+    model = Cart
+    list_display = ('user','staff', 'created_on',)    # here user__is_staff will not work   
+    list_filter = ('user', 'created_on',)
+    #fields = ('staff',)           # either fields or fieldset
+    fieldsets = (
+        (None, {'fields': ('user', 'created_on',)}),   # only direct relationship no nested relationship('__') ex. user__is_staff
+        #('User', {'fields': ('staff',)}),
+    )
+    inlines = (
+        ProductInCartInline,
+    )
+    # To display only in list_display
+    def staff(self,obj):
+        return obj.user.is_staff
+    # staff.empty_value_display = '???'
+    staff.admin_order_field  = 'user__is_staff'  #Allows column order sorting
+    staff.short_description = 'Staff User'  #Renames column head
+
+    #Filtering on side - for some reason, this works
+    list_filter = ['user__is_staff', 'created_on',]    # with direct foreign key(user) no error but not shown in filters, with function error
+    # ordering = ['user',]
+    search_fields = ['user__username']     # with direct foreign key no error but filtering not possible directly
+
+@admin.register(Deal)
+class DealAdmin(admin.ModelAdmin):
+    inlines = [
+        DealInline,
+    ]
+    
+    exclude = ('user',)
+
+
 admin.site.unregister(User)
 
 admin.site.register(User,UserAdmin)
 admin.site.register(Product)
-admin.site.register(Cart)
+
 admin.site.register(ProductInCart)
 admin.site.register(Order)
-admin.site.register(Deal)
