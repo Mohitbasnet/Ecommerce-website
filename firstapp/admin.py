1c918abe66f960a920e34cc1750b3e0401289975
banset <mohitbasnet80@gmail.com>
2024-04-03 14:08:03 +0545

    added customadminuser

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -5,6 +5,9 @@ from . models import Product,Cart,ProductInCart,Order,Deal
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
+from . models import CustomUser
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class CartInline(admin.TabularInline):
     model = Cart  #onetoonefield foreign key
@@ -15,34 +18,56 @@ class DealInline(admin.TabularInline):
 class ProductInCartInline(admin.TabularInline):
     model = ProductInCart
 
-class UserAdmin(UserAdmin):
-    model = User
-    list_display = ('username', 'get_cart', 'is_staff', 'is_active',)
-    list_filter = ('username', 'is_staff', 'is_active', 'is_superuser')
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Permissions', {'fields': ('is_staff', ('is_active' , 'is_superuser'), )}),
-        ('Important dates',{'fields': ('last_login', 'date_joined')}),
-        #('Cart', {'fields': ('get_cart',)})
-        ('Advanced options', {
-            'classes': ('collapse',), #Advance options
-            'fields': ('groups', 'user_permissions'),
-        }),
+        (None, {'fields': ('email',  'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser',)}),   #'is_customer' , 'is_seller'
     )
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),   # class for css 
-            'fields': ('username', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser', 'groups')}     # fields shown on create user page on admin panel
+            'classes': ('wide',),
+            'fields': ('email',  'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
+    search_fields = ('email',)
+    ordering = ('email',)
 
-    inlines = [
-        CartInline, DealInline
-    ]
-    def get_cart(self,obj):       # this function only works in list_display
-        return obj.cart           # through reverse related relationship
-    search_fields = ('username',)     #search_filter for search bar
-    ordering = ('username',)
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# This was for default UserAdmin
+# class UserAdmin(UserAdmin):
+#     model = User
+#     list_display = ('username', 'get_cart', 'is_staff', 'is_active',)
+#     list_filter = ('username', 'is_staff', 'is_active', 'is_superuser')
+#     fieldsets = (
+#         (None, {'fields': ('username', 'password')}),
+#         ('Permissions', {'fields': ('is_staff', ('is_active' , 'is_superuser'), )}),
+#         ('Important dates',{'fields': ('last_login', 'date_joined')}),
+#         #('Cart', {'fields': ('get_cart',)})
+#         ('Advanced options', {
+#             'classes': ('collapse',), #Advance options
+#             'fields': ('groups', 'user_permissions'),
+#         }),
+#     )
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),   # class for css 
+#             'fields': ('username', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser', 'groups')}     # fields shown on create user page on admin panel
+#         ),
+#     )
+
+#     inlines = [
+#         CartInline, DealInline
+#     ]
+#     def get_cart(self,obj):       # this function only works in list_display
+#         return obj.cart           # through reverse related relationship
+#     search_fields = ('username',)     #search_filter for search bar
+#     ordering = ('username',)
 
 @admin.register(Cart) # we can register cartadmin like this also
 class CartAdmin(admin.ModelAdmin):
@@ -78,9 +103,8 @@ class DealAdmin(admin.ModelAdmin):
     exclude = ('user',)
 
 
-admin.site.unregister(User)
 
-admin.site.register(User,UserAdmin)
+# admin.site.register(User,UserAdmin)
 admin.site.register(Product)
 
 admin.site.register(ProductInCart)
