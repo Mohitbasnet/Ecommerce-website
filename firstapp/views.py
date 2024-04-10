66492d54c9775cff5fab97e1fb669d92fe89aeeb
banset <mohitbasnet80@gmail.com>
2024-04-10 15:47:16 +0545

    change is over

--- firstapp/views.py
+++ firstapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 from django.views.generic import TemplateView
 
-
+from django.core.exceptions import ValidationError
 
 # def index(request):
 #     return render(request,'firstapp/index.html')
@@ -17,4 +17,28 @@ class Index(TemplateView):
         return context
 
 def contactus(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST['phone']
+        if len(phone)<10 or len(phone)>10:
+            raise ValidationError("Phone number length is not right")
+        query = request.POST['query']
+        print(name + " " + email + " " + phone + " " +query)
     return render(request, 'firstapp/contactus.html')
+
+def contactus2(request):
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():      #clean_data
+            if len(form.cleaned_data.get('query'))>10:
+                form.add_error('query', 'Query length is not right')
+                return render(request, 'firstapp/contactus2.html', {'form':form})
+            form.save()
+            return HttpResponse("Thank YOu")
+        else:
+            if len(form.cleaned_data.get('query'))>10:
+                #form.add_error('query', 'Query length is not right')
+                form.errors['__all__'] = 'Query length is not right. It should be in 10 digits.'
+            return render(request, 'firstapp/contactus2.html', {'form':form})
+    return render(request, 'firstapp/contactus2.html', {'form':ContactUsForm})
