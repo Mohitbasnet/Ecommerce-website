1df974f31772a7bf40c8b86f5635279577410fcb
banset <mohitbasnet80@gmail.com>
2024-04-21 23:23:45 +0545

    added multiple users functionality

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     # path('contactus/', views.contactus2, name = 'contact'),
     path('contactus/', views.ContanctUs.as_view(), name= 'contactclass'),
     path('signup/',views.RegisterView.as_view(), name = "signup"),
+    path('signup/', views.RegisterView.as_view(), name="signup"),
+    path('login/', views.LoginViewUser.as_view(), name="login"),
+    path('logout/', views.LogoutViewUser.as_view(), name="logout"),
+    path('signupseller/', views.RegisterViewSeller.as_view(), name= "login1"),
 ]
--- firstproject/settings.py
+++ firstproject/settings.py
@@ -129,4 +129,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
 
